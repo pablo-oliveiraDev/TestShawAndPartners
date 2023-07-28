7fcf17c51e8475bb14614805f98a5dcdd34aa7e1
pablo oliveira <pabloliverfe@gmail.com>
2023-07-28 20:46:17 -0300

    retirado console-log do findusercolumn

--- src/Controllers/users/FindUserAnyColumnController.ts
+++ src/Controllers/users/FindUserAnyColumnController.ts
@@ -5,7 +5,7 @@ import { prismaClient } from '../../database/prismaClient';
 export class FindUserAnyColumnController {
     async handle(request: Request, response: Response) {
         const column = request.query.column as string;
-        console.log(column)
+        
         const users = await prismaClient.user.findMany({
             where: {
                 OR: [
